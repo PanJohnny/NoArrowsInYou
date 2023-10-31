3d24546547869ca01f4935ac2e5c43d9b28150c2
PanJohnny <devstefancz@gmail.com>
2023-10-31 18:40:11 +0100

    Closes #6 I forgot to make the Inject cancellable

--- src/main/java/com/panjohnny/naiy/mixin/entity/WolfCollarFeatureMixin.java
+++ src/main/java/com/panjohnny/naiy/mixin/entity/WolfCollarFeatureMixin.java
@@ -4,7 +4,6 @@ import com.panjohnny.naiy.NAIY;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.WolfCollarFeatureRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.WolfEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WolfCollarFeatureRenderer.class)
 public class WolfCollarFeatureMixin {
-    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/passive/WolfEntity;FFFFFF)V", at = @At("HEAD"))
+    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/passive/WolfEntity;FFFFFF)V", at = @At("HEAD"), cancellable=true)
     public void naiy$render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, WolfEntity wolfEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
         if(NAIY.getConfig().hideWolfCollar) {
             ci.cancel();
